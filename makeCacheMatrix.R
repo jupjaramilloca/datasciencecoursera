e3b4a5571d4e1eb603c486bad9b4154d55085e66
Juan Pablo Jaramillo Castrillon <jupjaramilloca@unal.edu.co>
2020-09-08 09:27:09 -0500

    Update makeCacheMatrix.R
    Escribí comentarios sobre el código, en la parte posterior de cada linea.

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,3 +1,5 @@
+# Partimos de una nueva función
+
 makeCacheMatrix <- function(x= matrix()){
         inv <- NULL
         set <- function(y){
@@ -5,19 +7,21 @@ makeCacheMatrix <- function(x= matrix()){
                 inv <<- NULL
         }
         get <- function(){x}
-        setInverse <- function(inverse){inv <<- inverse}
-        getInverse <- function(){inv}
-        list(set=set,get=get, setInverse= setInverse, getInverse= getInverse)
+        setInverse <- function(inverse){inv <<- inverse}   ## SetInverse es como se llama la función para la inversa 
+        getInverse <- function(){inv}                      
+        list(set=set,get=get, setInverse= setInverse, getInverse= getInverse) # Una lista para las entradas de la función
 }
+
+
 ## Funcion CacheSolve
 cacheSolve <- function(x, ...){
         inv <- x$getInverse()
         if(!is.null(inv)){
-                message("getting cached data")
+                message("getting cached data")    ## Un mensaje cuando la inversa es Nula 
                 return(inv)
         }
         mat <- x$get()
-        inv <- solve(mat, ...)
+        inv <- solve(mat, ...)       ## Esto ayuda para sacar la inversa de la matriz.
         x$setInverse(inv)
         inv
 }
